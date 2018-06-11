a81eb457dfc26992a6edab21af741fba4e6972b1
wangxy2013 <wangxianyun1@163.com>
2018-06-11 16:02:32 +0800

    commint

--- app/src/main/java/com/zb/wyd/activity/MainActivity.java
+++ app/src/main/java/com/zb/wyd/activity/MainActivity.java
@@ -3,20 +3,26 @@ package com.zb.wyd.activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.shuyu.gsyvideoplayer.GSYVideoManager;
+import com.shuyu.gsyvideoplayer.listener.VideoAllCallBack;
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 import com.zb.wyd.R;
+import com.zb.wyd.utils.LogUtil;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity
 {
 
     @BindView(R.id.detail_player)
-    com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer videoPlayer;
-
+    StandardGSYVideoPlayer videoPlayer;
+    @BindView(R.id.btn_skip)
+    Button                 btnSkip;
 
     private OrientationUtils orientationUtils;
 
@@ -35,15 +41,26 @@ public class MainActivity extends BaseActivity
     @Override
     protected void initEvent()
     {
-
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                videoPlayer.setUp("http://vod3.vxinda.com/20180523/1381/playlist.m3u8", true, "测试视频");
+                videoPlayer.startPlayLogic();
+            }
+        });
     }
 
     @Override
     protected void initViewData()
     {
         String source1 = "http://vod3.vxinda.com/20180523/1381/playlist.m3u8";
-        videoPlayer.setUp(source1, true, "测试视频");
 
+        String source2 = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+
+        String rtsp = "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov";
+
+        videoPlayer.setUp(source2, true, "测试视频");
         //增加封面
         ImageView imageView = new ImageView(this);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
@@ -59,7 +76,7 @@ public class MainActivity extends BaseActivity
         videoPlayer.getFullscreenButton().setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v)
+            public void onClick(View view)
             {
                 orientationUtils.resolveByClick();
             }
@@ -76,6 +93,166 @@ public class MainActivity extends BaseActivity
             }
         });
         videoPlayer.startPlayLogic();
+
+
+        videoPlayer.setVideoAllCallBack(new VideoAllCallBack()
+        {
+            //开始加载，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onStartPrepared(String s, Object... objects)
+            {
+
+            }
+
+            //加载成功，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onPrepared(String s, Object... objects)
+            {
+
+            }
+
+            //点击了开始按键播放，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickStartIcon(String s, Object... objects)
+            {
+
+            }
+
+            //点击了错误状态下的开始按键，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickStartError(String s, Object... objects)
+            {
+
+            }
+
+            //点击了播放状态下的开始按键--->停止，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickStop(String s, Object... objects)
+            {
+
+            }
+
+            //点击了暂停状态下的开始按键--->播放，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickStopFullscreen(String s, Object... objects)
+            {
+
+            }
+
+            //点击了暂停状态下的开始按键--->播放，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickResume(String s, Object... objects)
+            {
+
+            }
+
+            //点击了全屏暂停状态下的开始按键--->播放，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickResumeFullscreen(String s, Object... objects)
+            {
+
+            }
+
+            //点击了空白弹出seekbar，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickSeekbar(String s, Object... objects)
+            {
+
+            }
+
+            //点击了全屏的seekbar，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickSeekbarFullscreen(String s, Object... objects)
+            {
+
+            }
+
+
+            //播放完了，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onAutoComplete(String s, Object... objects)
+            {
+
+            }
+
+
+            //进去全屏，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onEnterFullscreen(String s, Object... objects)
+            {
+
+            }
+
+            //退出全屏，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onQuitFullscreen(String s, Object... objects)
+            {
+
+            }
+
+            //进入小窗口，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onQuitSmallWidget(String s, Object... objects)
+            {
+
+            }
+
+            //退出小窗口，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onEnterSmallWidget(String s, Object... objects)
+            {
+
+            }
+
+            //触摸调整声音，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onTouchScreenSeekVolume(String s, Object... objects)
+            {
+
+            }
+
+            //触摸调整进度，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onTouchScreenSeekPosition(String s, Object... objects)
+            {
+
+            }
+
+            //触摸调整亮度，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onTouchScreenSeekLight(String s, Object... objects)
+            {
+
+            }
+
+            //播放错误，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onPlayError(String s, Object... objects)
+            {
+                LogUtil.e("TAG", "播放错误111111111111111111111111111111111111111111111111111");
+            }
+
+            //点击了空白区域开始播放，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickStartThumb(String s, Object... objects)
+            {
+
+            }
+
+            //点击了播放中的空白区域，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickBlank(String s, Object... objects)
+            {
+
+            }
+
+            //点击了全屏播放中的空白区域，objects[0]是title，object[1]是当前所处播放器（全屏或非全屏）
+            @Override
+            public void onClickBlankFullscreen(String s, Object... objects)
+            {
+
+            }
+        });
     }
 
 
@@ -116,4 +293,11 @@ public class MainActivity extends BaseActivity
         super.onBackPressed();
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
